f6fd9ba418eeebd72969380bb3341df89c7d3ae3
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-10-24 15:02:21 -0300

    Portifolio

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3080;
+
+app.get('/', (req, res) => {
+  res.json(
+    {
+      sucess: true,
+    }
+  );
+});
+
+app.get('/portifolio', (req, res) => {
+  const data = [
+    {
+      id: 1,
+      name: "Portifolio",
+      createdAt: "24-10-2022",
+    },
+    {
+      id: 2,
+      name: "Portifolio",
+      createdAt: "24-10-2022",
+    },
+    {
+      id: 3,
+      name: "Portifolio",
+      createdAt: "24-10-2022",
+    },
+];
+
+  res.json(
+    {
+      sucess: true,
+      data: data,
+    }
+  );
+});
+
+app.listen(PORT);
