d0cb5865c9ef53ee2675e377c52520973e711904
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-10-26 17:35:48 -0300

    models

---
+++ server/models/Portifolio.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const portifolioSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 1000
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model("portifolio", portifolioSchema);
