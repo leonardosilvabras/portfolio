1846baaf48eef02a1087bb35e5e299846f9a5f3d
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-10-28 09:58:59 -0300

    slug

--- server/models/Portifolio.js
+++ server/models/Portifolio.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const slug = require("slug");
 
 const portifolioSchema = new Schema({
   title: {
     type: String,
     required: true,
     maxlength: 100,
+    unique: true,
   },
   description: {
     type: String,
     required: true,
     maxlength: 1000,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    default: function () {
+      return slug(this.title);
+    },
   },
   createdAt: {
     type: Date,
