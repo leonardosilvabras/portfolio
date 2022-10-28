1bec551de69ac214ba41ce3944bac5cc623bef3c
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-10-28 10:07:38 -0300

    router slug

--- server/routes/portifolio.js
+++ server/routes/portifolio.js
@@ -16,6 +16,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:slug", async (req, res) => {
+  try {
+    const portifolio = await Portifolio.findOne({ slug: req.params.slug });
+
+    res.json({
+      sucess: true,
+      data: portifolio,
+    });
+  } catch (err) {
+    res.json({
+      sucess: false,
+      message: err,
+    });
+  }
+});
+
 router.post("/", async (req, res) => {
   const portifolio = new Portifolio({
     title: req.body.title,
