7786cc59b94c6164f9fa2f66b48b27fd3ec2cbc4
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2022-10-29 14:30:54 -0300

    Component Nav e fonte adicionada.

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,25 +1,65 @@
-import {PrimaryColor} from '../colors'
+import styled from 'styled-components'
+import { PrimaryColor } from "../colors";
 
 const Item = (name) => {
-    return (
-        <li>
-            <span style={{ color: PrimaryColor }}>
-                #
-            </span>
-            {name}
-        </li>
-    )
-}
+  return (
+    <Button>
+      <a href='#'>
+        <span style={{ color: PrimaryColor }}>#</span>
+        {name}
+      </a>
+    </Button>
+  );
+};
 
 const Nav = () => {
-    return (
-        <ul>
-            {Item("Inicio")}
-            {Item("Sobre")}
-            {Item("Projetos")}
-            {Item("Contato")}
-        </ul>
-    )
-}
+  return (
+    <Menu>
+      {Item("Inicio")}
+      {Item("Sobre")}
+      {Item("Projetos")}
+      {Item("Contato")}
+    </Menu>
+  );
+};
+
+const Menu = styled.div`
+  position: fixed;
+  top: 25%;
+  right: 0;
+
+  height: 50%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  list-style: none;
+`;
+
+const Button = styled.li`
+  width: 92px;
+  height: 30px;
+  margin: 3rem;
+
+  font-family: 'Alatsi';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+
+  text-transform: uppercase;
+
+  transform: rotate(90deg);
+
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default Nav;
